b489e422feeb88a36dcd9add0fb5e83cab366786
mariem <mariemmoalla@outlook.com>
2024-05-13 09:40:41 +0200

    Add project code

---
+++ ContactsApp/Models/TelemetryConstants.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics;
+
+namespace ContactsApp.Models;
+
+public static class TelemetryConstants
+{
+    /// <summary>
+    /// The name of the ActivitySource/Tracer that is going to produce our TRACES and
+    /// the Meter that is going to produce our METRICS.
+    /// </summary>
+    public const string MyAppSource = "ContactsApp";
+
+    public static readonly ActivitySource DemoTracer = new ActivitySource(MyAppSource);
+
+    public static readonly Meter DemoMeter = new Meter(MyAppSource);
+
+    public static readonly Counter<long> HitsCounter =
+        DemoMeter.CreateCounter<long>("IndexHits", "hits", "number of hits to homepage");
+}
---
+++ ContactsApp/Program.cs
@@ -0,0 +1,77 @@
+using ContactsApp.Data;
+using ContactsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Globalization;
+
+
+// shared Resource to use for both OTel metrics AND tracing (shared data)
+//telling otel that the exported traces all belong to Demo.AspNet
+var resource = ResourceBuilder.CreateDefault().AddService("ContactsApp");
+
+// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+//inject service for logging
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(TelemetryConstants.MyAppSource))
+        .AddConsoleExporter();
+});
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(TelemetryConstants.MyAppSource)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: TelemetryConstants.MyAppSource))
+    .AddConsoleExporter()
+    .Build();
+
+//inject service for tracing and metrcs
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(TelemetryConstants.MyAppSource))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddSqlClientInstrumentation()
+          .AddConsoleExporter().AddOtlpExporter(opts => opts.Endpoint = new Uri("http://localhost:4317")))
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddConsoleExporter());
+
+builder.Services.AddSingleton(TracerProvider.Default.GetTracer(TelemetryConstants.MyAppSource));
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ContactsAppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
